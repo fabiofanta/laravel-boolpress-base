be50fdde7b81fffe3d0dd583b4af4acd791bf0c1
fabiofanta <f.fantacuzzi@gmail.com>
2020-05-20 18:28:16 +0200

    10-comm

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,6 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
 
 Route::get('/posts', 'PostController@index')->name('posts.index');
